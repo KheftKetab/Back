89e750910a0fe3cb64324b36285735f8bce99284
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-12-10 12:36:24 +0330

    account model created

--- back/apps/accounts/models.py
+++ back/apps/accounts/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    university = models.CharField(
+        default="",
+        max_length=100,
+    )
+
+    phone_no = models.CharField(
+        default="",
+        max_length=100,
+    )
+
+    profile_picture = models.ImageField(
+        upload_to="../../static/profile_pictures/"
+    )
+
+    is_deleted = models.BooleanField(
+        default=False,
+    )
