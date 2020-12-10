3958c267d5e7c7e1a7e1f484ebbd3ea843fa7a6e
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2020-12-10 22:53:23 +0330

    account admin model created

--- back/apps/accounts/admin.py
+++ back/apps/accounts/admin.py
@@ -3,4 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Account
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'get_username',
+        'get_email',
+        'university',
+        'phone_no',
+    ]
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_username.short_description = "username"
+    get_email.short_description = "email"
